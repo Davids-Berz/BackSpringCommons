8faf8302812dbe902a36cb4a62828883c451bf1b
Davids.berz <Davids.berz@gmail.com>
2020-01-06 17:20:26 -0600

    commons curso back spring

---
+++ src/test/java/com/angular/backend/commons/SpringbootBackendCommonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angular.backend.commons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBackendCommonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
